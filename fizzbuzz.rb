bac3bdb5b26760ce95f16167e11fcfc69a2c3028
Brian Barnes <brianb@rueziffra.com>
2019-05-02 00:59:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
     if int % 3 == 0
-        return "Fizz"
-    elsif int % 5 == 0
-        return "Buzz"
+        "Fizz"
+    if int % 5 == 0
+        "Buzz"
     if int % 3 == 0 && int % 5 ==0
-       return "FizzBuzz"
+        "FizzBuzz"
     end
 end
